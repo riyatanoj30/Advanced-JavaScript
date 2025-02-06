e13a0585330e5df15c54b0e4a991c5f6829ef888
Riya Mhatre <riyatmhatre@gmail.com>
2025-02-07 00:25:56 +0530

    First commit of Array

---
+++ arr.js
@@ -0,0 +1,16 @@
+let arr=[];
+console.log(arr);
+
+let arr1=[10, 20, 30, 40];
+console.log(arr1);
+
+let a=new Array(10,20, 30);
+console.log(a);
+
+let b=["HTML", "CSS", "JS"];
+console.log(b);
+b[0]="Bootstrap";
+console.log(b);
+b.push("Node.js");
+b.unshift("Angular js");
+console.log(b);
