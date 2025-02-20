1f0f9cca35acfab3650753835aed51b7e1c92a28
Riya Mhatre <riyatmhatre@gmail.com>
2025-02-20 18:25:51 +0530

    Mergered 2 examples of even and odd

--- Even_Odd2.js
+++
@@ -1,9 +0,0 @@
-num = 5
-if(num % 2 == 0)
-{
-    console.log("Even");
-}
-else
-{
-    console.log("Odd");
-}
--- even_odd.js
+++ even_odd.js
@@ -1,6 +1,16 @@
-let num = 25;
-if(num % 2 == 0){
-    document.write("Even");
-}else{
-    document.write("Odd");
+// let num = 25;
+// if(num % 2 == 0){
+//     document.write("Even");
+// }else{
+//     document.write("Odd");
+// }
+
+num = 5
+if(num % 2 == 0)
+{
+    console.log("Even");
+}
+else
+{
+    console.log("Odd");
 }
